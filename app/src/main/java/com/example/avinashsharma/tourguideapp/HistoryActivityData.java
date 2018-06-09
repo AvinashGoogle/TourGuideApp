439d901362558f4d0da9440da78b0ae7561b82e8
AvinashGoogle <mastavinash@gmail.com>
2018-06-10 02:23:06 +0530

    Initial commit

---
+++ app/src/main/java/com/example/avinashsharma/tourguideapp/HistoryActivityData.java
@@ -0,0 +1,11 @@
+package com.example.avinashsharma.tourguideapp;
+
+import android.content.Context;
+
+import java.util.List;
+
+public class HistoryActivityData {
+    public static void initHistory(List<Data> list, Context context){
+        list.add(new Data(R.string.history_title ,R.string.history_body));
+    }
+}
---
+++ app/src/main/java/com/example/avinashsharma/tourguideapp/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.avinashsharma.tourguideapp;
+
+import android.net.Uri;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity implements FoodActivity.OnFragmentInteractionListener,
+        HistoryActivity.OnFragmentInteractionListener, HotelsActivity.OnFragmentInteractionListener,
+        SightsActivity.OnFragmentInteractionListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TabLayout tabLayout = (TabLayout)findViewById(R.id.tabLayout);
+        tabLayout.addTab(tabLayout.newTab().setText("Food"));
+        tabLayout.addTab(tabLayout.newTab().setText("Hotels"));
+        tabLayout.addTab(tabLayout.newTab().setText("Sights"));
+        tabLayout.addTab(tabLayout.newTab().setText("History"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final ViewPager viewPager = (ViewPager)findViewById(R.id.viewPager);
+        final PagerAdapter pagerAdapter = new PagerAdapter(getSupportFragmentManager(),tabLayout.getTabCount());
+        viewPager.setAdapter(pagerAdapter);
+
+        viewPager.setOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+}
